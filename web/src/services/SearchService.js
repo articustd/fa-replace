2020d4a44845079f3289407d87d0f39c913dcfa8
Articus <articusthedata@gmail.com>
2022-11-21 19:32:42 -0800

    Added published date to test data

--- web/src/services/SearchService.js
+++ web/src/services/SearchService.js
@@ -2,6 +2,6 @@ import _ from 'lodash'
 
 export function find(numEntries) {
     return _.times(numEntries, (num)=>{
-        return {title: `Test Title ${num}`, author: "Test Author", tags: ["Test Tag 1", "Test Tag 2", "Test Tag 3"], short_desc: "This is a short description", word_count: 500}
+        return {title: `Test Title ${num}`, author: "Test Author", tags: ["Test Tag 1", "Test Tag 2", "Test Tag 3"], short_desc: "This is a short description", word_count: 500, published_date: "Nov 20th, 2022"}
     })
 }
